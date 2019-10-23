ae8ffa6141ea4d89121abab181a09ec9b512a9b2
Nethelbert <gilblasse@gmail.com>
2019-10-23 15:05:24 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -18,8 +18,9 @@ def literal_aoh
   my_array = [
       { name:  "Don Gately", occupation: "Live-in Staff" },
       { name:  "Joelle van Dyne", occupation: "Radio Personality" },
-      
-      
+      { name:  "Pat Monteseian", occupation: "Staff" },
+      { name:  "Kate Gompert", occupation: "None" },
+      { name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
 end
 
